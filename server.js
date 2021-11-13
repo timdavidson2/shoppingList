bfd6feb60a57eb35c3bf3e22af9e90ddd93c92f0
Tim Davidson <timdavidson2@gmail.com>
2021-11-13 17:08:42 -0500

    finish server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 app.use(express.json());
@@ -13,6 +15,9 @@ mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected....'))
   .catch((err) => console.log(err));
+
+// use routes
+app.use('/api/items', items);
 
 const port = process.env.PORT || 3001;
 
